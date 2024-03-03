52e778b283b779039e57493ad7659c938fa486f4
Kim Changhyun <ok2qkq@naver.com>
2024-03-03 17:20:53 +0900

    #2-사용자 계정 repo layer와 테스트 로직 작성

---
+++ project-board-admin/project-board-admin/src/main/java/com/fastcampus/projectboardadmin/config/JpaConfig.java
@@ -0,0 +1,29 @@
+package com.fastcampus.projectboardadmin.config;
+
+import com.fastcampus.projectboardadmin.dto.security.BoardAdminPrincipal;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardAdminPrincipal.class::cast)
+                .map(BoardAdminPrincipal::getUsername);
+        // 기존의 로직은 인증완료된 Principal에서 username을 가져와야 하기 때문에 회원가입할 때 유저정보를 입력 불가능.
+    }
+
+}
