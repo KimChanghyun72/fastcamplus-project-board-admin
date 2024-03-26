a57ce7711f5b8be68358f880c426f32cc09a9acc
Kim Changhyun <ok2qkq@naver.com>
2024-03-26 16:51:46 +0900

    #18-UserAccount 객체의 이름을 AdminAccount 로 변경
    UserAccount 명칭이 관리자 계정인지 게시판 서비스에서 넘어온 사용자계정인지
    혼동이 올 수 있으므로 관리자계정 객체의 이름을 AdminAccount로 변경하고
    관련된 테이블 및 코드의 내용을 변경

--- project-board-admin/project-board-admin/src/main/java/com/fastcampus/projectboardadmin/dto/security/BoardAdminPrincipal.java
+++ project-board-admin/project-board-admin/src/main/java/com/fastcampus/projectboardadmin/dto/security/BoardAdminPrincipal.java
@@ -1,8 +1,7 @@
 package com.fastcampus.projectboardadmin.dto.security;
 
 import com.fastcampus.projectboardadmin.domain.constant.RoleType;
-import com.fastcampus.projectboardadmin.dto.UserAccountDto;
-import lombok.Getter;
+import com.fastcampus.projectboardadmin.dto.AdminAccountDto;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -45,7 +44,7 @@ public record BoardAdminPrincipal(
         );
     }
 
-    public static BoardAdminPrincipal from(UserAccountDto dto) {
+    public static BoardAdminPrincipal from(AdminAccountDto dto) {
         return BoardAdminPrincipal.of(
                 dto.userId(),
                 dto.userPassword(),
@@ -56,8 +55,8 @@ public record BoardAdminPrincipal(
         );
     }
 
-    public UserAccountDto toDto() {
-        return UserAccountDto.of(
+    public AdminAccountDto toDto() {
+        return AdminAccountDto.of(
                 username,
                 password,
                 authorities().stream()
