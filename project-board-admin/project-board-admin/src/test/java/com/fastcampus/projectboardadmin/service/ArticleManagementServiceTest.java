35c026afbacdbd5edda9c64e85935362d10c4c33
Kim Changhyun <ok2qkq@naver.com>
2024-03-26 14:45:24 +0900

    # 단위테스트 정의 및 테스트에 맞춰 설정 추가

---
+++ project-board-admin/project-board-admin/src/test/java/com/fastcampus/projectboardadmin/service/ArticleManagementServiceTest.java
@@ -0,0 +1,192 @@
+package com.fastcampus.projectboardadmin.service;
+
+
+import com.fastcampus.projectboardadmin.domain.constant.RoleType;
+import com.fastcampus.projectboardadmin.dto.ArticleDto;
+import com.fastcampus.projectboardadmin.dto.UserAccountDto;
+import com.fastcampus.projectboardadmin.dto.properties.ProjectProperties;
+import com.fastcampus.projectboardadmin.dto.response.ArticleClientResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+
+@ActiveProfiles("test")     // ?
+@DisplayName("비즈니스 로직 - 게시글 관리")
+class ArticleManagementServiceTest {
+
+    // 실제 환경 테스트
+    @Disabled("실제 API 호출 결과 관찰용이므로 평상시에는 비활성화.")       // 운영 환경에서 문제 발생했을 때만 돌리는 테스트라고 가정.
+    @DisplayName("실제 API 호출 테스트")
+    @SpringBootTest
+    @Nested
+    class RealApiTest {
+
+        private final ArticleManagementService sut;
+
+        @Autowired
+        public RealApiTest(ArticleManagementService sut) {
+            this.sut = sut;
+        }
+
+        @DisplayName("게시글 API를 호출하면, 게시글을 가져온다.")
+        @Test
+        void given_when_then() {
+            // Given
+
+            // When
+            List<ArticleDto> result = sut.getArticles();
+
+            // Then
+            // 응답되는지만 확인하면 되므로 간단히 첫번째 결과 하나만 추출해서 본다.
+            System.out.println(result.stream().findFirst());
+            assertThat(result).isNotNull();
+        }
+    }
+
+    // mock으로 테스트
+    @DisplayName(" API mocking 테스트")
+    @EnableConfigurationProperties(ProjectProperties.class)
+    @AutoConfigureWebClient(registerRestTemplate = true)        // 등록한 restTemplate 설정을 사용함.
+    @RestClientTest(ArticleManagementService.class)
+    @Nested
+    class RestTemplateTest {
+
+        private final ArticleManagementService sut;
+        //private final RestTemplate restTemplate;
+        private final ProjectProperties projectProperties;
+        private final MockRestServiceServer server;
+        private final ObjectMapper mapper;              // 직렬화, 역직렬화를 위한 설정
+
+        @Autowired
+        public RestTemplateTest(
+                ArticleManagementService sut,
+                ProjectProperties projectProperties,
+                MockRestServiceServer server,
+                ObjectMapper mapper) {
+            this.sut = sut;
+            this.projectProperties = projectProperties;
+            this.server = server;
+            this.mapper = mapper;
+        }
+
+//        void test() {
+//            restTemplate.getForObject()
+//        }
+
+        @DisplayName("게시글 목록 API를 호출하면, 게시글들을 가져온다.")
+        @Test
+        void givenNothing_whenCallingArticlesApi_thenReturnsArticleList() throws JsonProcessingException {
+            // Given
+            ArticleDto expectedArticle = createArticleDto("제목", "글");
+            ArticleClientResponse expectedResponse = ArticleClientResponse.of(List.of(expectedArticle));
+            server
+                    .expect(requestTo(projectProperties.board().url() + "/api/articles?size=10000"))
+                    .andRespond(MockRestResponseCreators.withSuccess(
+                            mapper.writeValueAsString(expectedResponse),
+                            MediaType.APPLICATION_JSON
+                    ));
+
+            // When
+            List<ArticleDto> result = sut.getArticles();
+
+            // Then
+            assertThat(result).first()
+                    .hasFieldOrPropertyWithValue("id", expectedArticle.id())
+                    .hasFieldOrPropertyWithValue("title", expectedArticle.title())
+                    .hasFieldOrPropertyWithValue("content", expectedArticle.content())
+                    .hasFieldOrPropertyWithValue("userAccount.nickname", expectedArticle.userAccount().nickname());
+            server.verify();        // 위의 내용이 실제로 이루어졌는지 검증.
+        }
+
+        @DisplayName("게시글 ID와 함께 게시글 API을 호출하면, 게시글을 가져온다.")
+        @Test
+        void givenArticleId_whenCallingArticleApi_thenReturnsArticle() throws Exception {
+            // Given
+            Long articleId = 1L;
+            ArticleDto expectedArticle = createArticleDto("게시판", "글");
+            server
+                    .expect(requestTo(projectProperties.board().url() + "/api/articles/" + articleId))
+                    .andRespond(withSuccess(
+                            mapper.writeValueAsString(expectedArticle),
+                            MediaType.APPLICATION_JSON
+                    ));
+
+            // When
+            ArticleDto result = sut.getArticle(articleId);
+
+            // Then
+            assertThat(result)
+                    .hasFieldOrPropertyWithValue("id", expectedArticle.id())
+                    .hasFieldOrPropertyWithValue("title", expectedArticle.title())
+                    .hasFieldOrPropertyWithValue("content", expectedArticle.content())
+                    .hasFieldOrPropertyWithValue("userAccount.nickname", expectedArticle.userAccount().nickname());
+            server.verify();
+        }
+
+        @DisplayName("게시글 ID와 함께 게시글 삭제 API을 호출하면, 게시글을 삭제한다.")
+        @Test
+        void givenArticleId_whenCallingDeleteArticleApi_thenDeletesArticle() throws Exception {
+            // Given
+            Long articleId = 1L;
+            server
+                    .expect(requestTo(projectProperties.board().url() + "/api/articles/" + articleId))
+                    .andExpect(method(HttpMethod.DELETE))
+                    .andRespond(withSuccess());
+
+            // When
+            sut.deleteArticle(articleId);
+
+            // Then
+            server.verify();
+        }
+
+        private ArticleDto createArticleDto(String title, String content) {
+            return ArticleDto.of(
+                    1L,
+                    createUserAccountDto(),
+                    title,
+                    content, null,
+                    LocalDateTime.now(),
+                    "Uno",
+                    LocalDateTime.now(),
+                    "Uno"
+            );
+        }
+
+        private UserAccountDto createUserAccountDto() {
+            return UserAccountDto.of(
+                    "unoTest",
+                    "pw",
+                    Set.of(RoleType.ADMIN),
+                    "uno-test@email.com",
+                    "uno-test",
+                    "test memo"
+            );
+        }
+
+    }
+
+}
